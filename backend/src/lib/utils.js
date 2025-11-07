5f8d42ad9e3b8caa309c648e290296e04120006d
cuongdata <nguyenchicuongpy2021@gmail.com>
2025-11-07 22:55:59 +0700

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-//   const { JWT_SECRET } = ENV;
-//   if (!JWT_SECRET) {
-//     throw new Error("JWT_SECRET is not configured");
-//   }
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
 
   const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
