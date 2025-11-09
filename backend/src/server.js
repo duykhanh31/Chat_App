1222d8f3f1f1e59c7d69254843b2d99c6d9b8341
Khiemnt0403 <thanhkhiem2k99@gmail.com>
2025-11-09 18:13:20 +0700

    add login, signup, chatpage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import path from 'path';  
+import path from 'path'; 
+import cors from "cors";
+
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -16,6 +18,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
