3a224430382ab7b978e1a4e2c54fd25cb850b242
LeDucQuangHuy <huyldq8395@ut.edu.vn>
2025-11-10 12:21:53 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { app`} from "./lib/socket.js";
 // import { app, server } from "./lib/socket.js";
 const __dirname = path.resolve();
 
