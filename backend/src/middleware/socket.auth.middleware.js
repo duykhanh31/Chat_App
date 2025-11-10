50407a1e9122872226dcfdff3dc07e61f90c7719
LeDucQuangHuy <huyldq8395@ut.edu.vn>
2025-11-10 09:11:03 +0700

    Create socket.auth.middleware.js

---
+++ backend/src/middleware/socket.auth.middleware.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { ENV } from "../lib/env.js";
+
+export const socketAuthMiddleware = async (socket,next) => {
+  
+};
+  
