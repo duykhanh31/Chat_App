d9b304a7b1792ecfdf0c9d64969c338dfb9afbdd
LeDucQuangHuy <huyldq8395@ut.edu.vn>
2025-11-10 09:05:07 +0700

    Create socket.js

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,17 @@
+import { Server } from "socket.io ";
+import http from "http";
+import express from "express";
+import { ENV } from "./env.js";
+
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: [ENV.CLIENT_URL],
+    credentials: true,
+  },
+});
+
+//apply authentication middleware to all socket connections
+io.use(socketAuthMiddleware
